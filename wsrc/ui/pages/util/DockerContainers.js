c8b2d7d282df1cafa229fe9049802c660321447c
floffah <therealfloffah@gmail.com>
2020-06-28 10:31:26 +0100

    container list style fix

--- wsrc/ui/pages/util/DockerContainers.js
+++ wsrc/ui/pages/util/DockerContainers.js
@@ -5,9 +5,9 @@
  *     @link https://github.com/floffah/
  */
 import React, {useState} from 'react';
-import {Tabs, PageHeader, Descriptions, Button, Tooltip} from "antd";
+import {Tabs, PageHeader, Descriptions, Button, Tooltip, Empty} from "antd";
 import {post} from "../../../lib/comms";
-import {CheckCircleFilled} from "@ant-design/icons";
+import {CheckCircleFilled, PauseCircleFilled} from "@ant-design/icons";
 
 export default function DockerContainers() {
     let [allc, setAllc] = useState(<div>Press refresh.</div>);
@@ -23,16 +23,23 @@ export default function DockerContainers() {
                 let dateC = new Date(0);
                 dateC.setUTCSeconds(container.Created);
 
-                let ports = `${container.Ports[0].Type}://${container.Ports[0].IP}:${container.Ports[0].PublicPort} (${container.Ports[0].PrivatePort})`;
-                container.Ports.forEach(port => {
-                    if(container.Ports.indexOf(port) !== 0) {
-                        ports += `, ${port.Type}://${port.IP}:${port.PublicPort} (${port.PrivatePort})`
-                    }
-                });
+                let ports;
+                if(container.Ports > 0) {
+                    ports = `${container.Ports[0].Type}://${container.Ports[0].IP}:${container.Ports[0].PublicPort} (${container.Ports[0].PrivatePort})`;
+                    container.Ports.forEach(port => {
+                        if(container.Ports.indexOf(port) !== 0) {
+                            ports += `, ${port.Type}://${port.IP}:${port.PublicPort} (${port.PrivatePort})`
+                        }
+                    });
+                } else {
+                    ports = "None";
+                }
 
                 let state = container.State;
                 if(state === "running") {
                     state = <span><CheckCircleFilled className="greenclr"/>&nbsp;Running</span>
+                } else if(state === "exited") {
+                    state = <span><PauseCircleFilled className="orangeclr"/>&nbsp;Exited</span>
                 }
 
                 toRender.push(<PageHeader style={{outline: "1px solid #f0f0f0", borderRadius: 5}} key={made} ghost={false} title={container.Names[0]}>
@@ -47,6 +54,9 @@ export default function DockerContainers() {
                 </PageHeader>);
                 made++;
             });
+            if(resp.data.containers.length === 0) {
+                toRender = <Empty/>
+            }
             setAllc(toRender);
         });
     }
